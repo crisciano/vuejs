4a21cbc3829455989181533dc85dde9a55cc323e
Crisciano S. Botelho <cris@apeppers.com.br>
2017-08-16 12:13:10 -0300

    livro e autor

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,15 @@ $(document).ready(function() {
 
 /* add click */
 function onAdd(){
-	let $li, $label, $div, $input;
+	let $li, $label, $div, $input, $big, $small;
 	let $list 	= $('.list-livros');
-	let el 		= $('.new-book');
-	let livro 	= $('.new-book').val();
+	let elBook	= $('.new-book');
+	let elAutor = $('.new-autor');
+	let livro 	= elBook.val();
+	let autor 	= elAutor.val();
 
 	// validação simples se o campo não esta vazio 
-	if (livro === '') { 
+	if (livro === '' && auto === '') { 
 		alert('campo vazio!!');
 		return; 
 	}
@@ -28,16 +30,26 @@ function onAdd(){
 	//cria o input
 	$input 	= $('<input>').attr({ type: 'checkbox',name: 'list'})
 		.addClass('js-livro').click(toggleRemovido);
+	// cria big
+	$big 	= $('<big>');
+	// cria small
+	$small 	= $('<small>');
 
 	// criar elementos ao inverso
+	$big.append(livro);  	/* inseri o valor do livro no elemento big */
+	$small.append(autor); 	/* inseri o valor do autor no elemento small  */
 	$label.append($input); 	/* inseri o elemento input dentro do label */
-	$label.append(livro); 	/* inseri o valor do input digitado no label */
+	$label.append($big); 	/* inseri o elemento big no elemento label */
+	$label.append(' - '); 	/* cria um elemento separador */
+	$label.append($small); 	/* inseri o elemento small no elemento label */
 	$div.append($label); 	/* inseri o label dentro da div */
 	$li.append($div); 		/* inseri o div dentro da li */
 	$list.append($li); 		/* inseri o li dentro da lista */
 
-	// limpa o campo do input
-	el.val('');
+	// limpa o campo do livro
+	elBook.val('');
+	// limpa o campo de autor
+	elAutor.val('');
 }
 
 // click do checkbox
