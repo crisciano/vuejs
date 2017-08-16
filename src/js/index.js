a4726d357636dcb1491b10bdedd74490a1936576
Crisciano S. Botelho <cris@apeppers.com.br>
2017-08-16 11:44:08 -0300

    normalizacao

--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 
 /* add click */
 function onAdd(){
-	let $li, $label, $div;
+	let $li, $label, $div, $input;
+	let $list 	= $('.list-livros');
 	let el 		= $('.new-book');
-	let list 	= $('.list-livros');
 	let livro 	= $('.new-book').val();
 
 	// validação simples se o campo não esta vazio 
@@ -16,21 +16,25 @@ function onAdd(){
 		alert('campo vazio!!');
 		return; 
 	}
-	
-	// inclui o elemento li dentro do ul
-	$li 	= $('<li>').appendTo(list);
-	// add class checkbox e inclui o elemento li dentro da div
-	$div 	= $('<div>').addClass('checkbox').appendTo($li); 
-	// add a div dentro do label
-	$label 	= $('<label>').appendTo($div);
 
-	$('<input>')
-		.attr({ type: 'checkbox', name: 'list' })
-		.addClass('js-livro')
-		.click(toggleRemovido)
-		.appendTo($label);
+	// list> li > div.checkbox > label > input
 
-	$label.append(livro);
+	//cria o elemento li 
+	$li 	= $('<li>');
+	// cria a div 
+	$div 	= $('<div>').addClass('checkbox');
+	// cria label
+	$label 	= $('<label>');
+	//cria o input
+	$input 	= $('<input>').attr({ type: 'checkbox',name: 'list'})
+		.addClass('js-livro').click(toggleRemovido);
+
+	// criar elementos ao inverso
+	$label.append($input); 	/* inseri o elemento input dentro do label */
+	$label.append(livro); 	/* inseri o valor do input digitado no label */
+	$div.append($label); 	/* inseri o label dentro da div */
+	$li.append($div); 		/* inseri o div dentro da li */
+	$list.append($li); 		/* inseri o li dentro da lista */
 
 	// limpa o campo do input
 	el.val('');
